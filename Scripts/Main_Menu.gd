ac0b5abe54f8245c6548eeeab80d90a4887d5249
Peyton Petersen <txy738@usask.ca>
2026-09-19 15:54:46 -0600

    please don't break anything :pray:

---
+++ Scripts/Main_Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	
+
+
+func _on_check_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+
+
+
+func _on_button_exit_pressed() -> void:
+	get_tree().quit()
