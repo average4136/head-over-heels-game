3670015be0590910c8143854967e8f89d9d8ca94
Hannah Tremaine <sta674@usask.ca>
2026-09-19 11:30:34 -0600

    Character currently has nice movement
    character is "flingable",  the weights and speeds feel right to me, need to stop foraceable downwards motion etc. and tweak but we're gettign there!

--- Scripts/body.gd
+++ Scripts/body.gd
@@ -1,48 +1,28 @@
 extends RigidBody2D
 
-
-# How strongly the player pushes the body horizontally.
-#
-# This is FORCE, not speed.
-# Bigger number = faster acceleration.
-@export var move_force: float = 10000.0
-
+@export var move_force: float = 15000.0
+@export var jump_force: float = 800.0
 
 func _physics_process(_delta: float) -> void:
-
-	# Returns:
-	# -1.0 when moving left
-	#  0.0 when doing nothing
-	# +1.0 when moving right
 	var input_direction := Input.get_axis(
 		"move_left",
 		"move_right"
 	)
 
-
-	# Only do something if the player is pressing a direction.
+	# Horizontal movement
 	if input_direction != 0.0:
-
-		# Wake the physics body if Godot allowed it to sleep.
 		sleeping = false
 
-
-		# Create a horizontal force.
-		#
-		# Pressing RIGHT:
-		# Vector2(5000, 0)
-		#
-		#              ------>
-		#
-		# Pressing LEFT:
-		# Vector2(-5000, 0)
-		#
-		#        <------
 		var movement_force := Vector2(
 			input_direction * move_force,
 			0.0
 		)
 
-
-		# Ask the physics engine to push the body.
 		apply_central_force(movement_force)
+
+	# Small controlled jump
+	if Input.is_action_just_pressed("jump"):
+		if $GroundRay.is_colliding():
+			apply_central_impulse(
+				Vector2(0.0, -jump_force)
+			)
