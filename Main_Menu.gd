4429d58104df0fcb7d67dd195442335fcd16e973
koolhundluke <koolhundluke@gmail.com>
2026-09-18 23:20:48 -0600

    changed settings error

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -6,4 +6,4 @@ func _on_button_pressed() -> void:
 
 
 func _on_check_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://")
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
