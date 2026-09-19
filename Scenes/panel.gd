777a5158fbab3410ecf909f926b2fa460957750f
koolhundluke <koolhundluke@gmail.com>
2026-09-19 11:27:43 -0600

    added initial scrip for the Timer

---
+++ Scenes/panel.gd
@@ -0,0 +1,21 @@
+extends Panel
+
+var time: float = 0.0
+var minutes: int = 0
+var seconds: int = 0
+var mseconds: int = 0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	time += delta
+	mseconds = fmod(time,1) * 100
+	seconds = fmod(time,60)
+	minutes = fmod(time,3600) / 60
+	$mseconds.text = "%02d:" % mseconds
+	$seconds.text = "%02d:" % seconds
+	$minutes.text = "%02d:" % minutes
+	
+		
+	
+		
+	
+	
---
+++ Scenes/popup_menu.gd
@@ -0,0 +1,5 @@
+extends PopupMenu
+
+
+func _on_popup_menu_id_pressed(id: int) -> void:
+	
