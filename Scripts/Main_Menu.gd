2177eb72fd8aabb141b018a2ed848163326b7315
Christian Quin <152404915+enchong-codes@users.noreply.github.com>
2026-09-19 19:26:25 -0600

    Update Main_Menu.gd

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Cutscenes/opening_cutscene.tscn")
 
 func _on_check_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
