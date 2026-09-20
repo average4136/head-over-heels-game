26a71fa2ac55ef11bcfdcec9fe01ceb37a74aa60
Christian Quin <152404915+enchong-codes@users.noreply.github.com>
2026-09-19 18:17:29 -0600

    ADD: ending_cutscene

---
+++ Scripts/ending_cutscene.gd
@@ -0,0 +1,20 @@
+extends "res://Scripts/cutscene_player.gd"
+
+func _setup_slides() -> void:
+	slides = [
+		{"text": "\"You made it boy.\"", "img": null, "audio": null, "time": 3.0},
+		{"text": "\"Will you cure me now?\"", "img": null, "audio": null, "time": 3.0},
+		{"text": "\"You've gotten really good with that head though, I feel like you should keep it\"", "img": null, "audio": null, "time": 4.0},
+		{"text": "...", "img": null, "audio": null, "time": 2.0},
+		{"text": "\"you're no fun. fine. walk through that portal. but hear my word, i cannot guarantee your syndrome to be fixed. that is truth. its up to you\"", "img": null, "audio": null, "time": 5.0},
+		{"text": "\"wait, WHAT?\"", "img": null, "audio": null, "time": 2.0},
+		{"text": "*Teleporting...*", "img": null, "audio": null, "time": 2.0},
+		{"text": "\"hey, excuse me! wait up!\"", "img": null, "audio": null, "time": 3.0},
+		{"text": "\"hm?\"", "img": null, "audio": null, "time": 2.0},
+		{"text": "\"shoot. i must look so stupid. i should say nevermind. she must be so disgusted\"", "img": null, "audio": null, "time": 4.0},
+		{"text": "*She turns around, smiling and listening intently.*", "img": null, "audio": null, "time": 3.5},
+		{"text": "\"i guess that stupid omni-whatever being was right...\"", "img": null, "audio": null, "time": 3.5},
+		{"text": "\"its all just in my head.\"", "img": null, "audio": null, "time": 3.0},
+		{"text": "\"i feel so light.\"", "img": null, "audio": null, "time": 3.0},
+		{"text": "<<<< Hi, my name is... >>>>\n\nDOWN BAD.", "img": null, "audio": null, "time": 5.0}
+	]
