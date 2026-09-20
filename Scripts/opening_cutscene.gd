9fe9b3b62e427c59e3058bcd6e60db5ef24c06b9
Christian Quin <152404915+enchong-codes@users.noreply.github.com>
2026-09-19 19:46:10 -0600

    Update opening_cutscene.gd

--- Scripts/opening_cutscene.gd
+++ Scripts/opening_cutscene.gd
@@ -2,11 +2,11 @@ extends "res://Scripts/cutscene_player.gd"
 
 func _setup_slides() -> void:
 	slides = [
-		{"text": "That day, I saw her.", "img": null, "audio": null, "time": 3.0},
+		{"text": "That day, I saw her.", "img": preload("res://Assets/Cutscenes/Opening_Cutscene_002.png"), "audio": null, "time": 3.0},
 		{"text": "But my feet wouldn't move.", "img": null, "audio": null, "time": 3.0},
 		{"text": "Negative thoughts kept filling my head...", "img": null, "audio": null, "time": 3.5},
 		{"text": "...and weighed me down.", "img": null, "audio": null, "time": 3.0},
-		{"text": "She was out of reach.", "img": null, "audio": null, "time": 3.0}
+		{"text": "She was out of reach.", "img": preload("res://Assets/Cutscenes/Opening_Cutscene_001.png"), "audio": null, "time": 3.0}
 	]
 
 # next scene path will be the post-eyeopen scene on the street
