143e02ce785c4f14dd3317cc1e0582d2badd7673
Christian Quin <152404915+enchong-codes@users.noreply.github.com>
2026-09-20 10:20:15 -0600

    opening cutscene OST added and slides

--- Scripts/opening_cutscene.gd
+++ Scripts/opening_cutscene.gd
@@ -1,12 +1,17 @@
 extends "res://Scripts/cutscene_player.gd"
 
+func _ready() -> void:
+	super._ready() # Calls cutscene_player.gd's ready setup first
+	if ost:
+		ost.play(30.5) # Starts playback at 14.5 seconds
+
 func _setup_slides() -> void:
 	slides = [
-		{"text": "That day, I saw her.", "img": preload("res://Assets/Cutscenes/Opening_Cutscene_002.png"), "audio": null, "time": 3.0},
-		{"text": "But my feet wouldn't move.", "img": null, "audio": null, "time": 3.0},
-		{"text": "Negative thoughts kept filling my head...", "img": null, "audio": null, "time": 3.5},
-		{"text": "...and weighed me down.", "img": null, "audio": null, "time": 3.0},
-		{"text": "She was out of reach.", "img": preload("res://Assets/Cutscenes/Opening_Cutscene_001.png"), "audio": null, "time": 3.0}
+		{"text": "That day, I saw her.", "img": preload("res://Assets/Cutscenes/Opening_Cutscene_V0.5.png")},
+		{"text": "But my feet wouldn't move.", "img": preload("res://Assets/Cutscenes/Op_Scene_03_stand.png")},
+		{"text": "Negative thoughts kept filling my head...", "img": preload("res://Assets/Cutscenes/Boy_No_Face_0.5.png")},
+		{"text": "...and weighed me down.", "img": preload("res://Assets/Cutscenes/Boy_Brain_scared.png")},
+		{"text": "She was out of reach.", "img": preload("res://Assets/Cutscenes/Opening_Cutscene_001.png")},
+		{"text": "", "img": preload("res://Assets/Cutscenes/Boy_Brain_overwhelmed.png")},
+		{"text": "", "img": null}
 	]
-
-# next scene path will be the post-eyeopen scene on the street
