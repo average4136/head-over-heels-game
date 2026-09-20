9a8c13d143d2aa439c79e1c094ed1d965142a125
MrTThunderr <110064560+MrTThunderr@users.noreply.github.com>
2026-09-20 15:48:31 -0600

    ewfewfwef

--- Scripts/win_portal.gd
+++ Scripts/win_portal.gd
@@ -5,11 +5,11 @@ extends Area2D
 var triggered: bool = false
 
 func _ready() -> void:
-	MenuTheme.stop_music_level() # disable song level
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
 	if triggered:
+		MenuTheme.stop_music_level() # disable song level
 		return
 
 	if body.is_in_group("player") or body.name in ["Guy", "char", "Head", "Body"] or (body.get_parent() and "Guy" in body.get_parent().name):
