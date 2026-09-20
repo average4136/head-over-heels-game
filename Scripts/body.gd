01a2ec85ff0f4aad21c3ff00f9fe7968e7f5e196
MrTThunderr <110064560+MrTThunderr@users.noreply.github.com>
2026-09-20 11:23:23 -0600

    sfx changes and guy changes

--- Scripts/body.gd
+++ Scripts/body.gd
@@ -4,21 +4,31 @@ extends RigidBody2D
 @export var move_force: float = 15000.0
 @export var jump_force: float = 1400.0
 
+@onready var step_player: AudioStreamPlayer = $step
+@onready var ground_ray: RayCast2D = $GroundRay
+
+var canPlay = true
+var timer = 0.25
+var setTimer = 0.25
+
 func _physics_process(_delta: float) -> void:
-	var input_direction := Input.get_axis(
-		"move_left",
-		"move_right"
-	)
+	var input_direction := Input.get_axis("move_left", "move_right")
 
 	# Horizontal movement
 	if input_direction != 0.0:
 		sleeping = false
-
-		var movement_force := Vector2(
-			input_direction * move_force,
-			0.0
-		)
-
+		
+		# SFX
+		if canPlay: 
+			if ground_ray.is_colliding(): step_player.play()
+			canPlay = false
+			timer = setTimer
+		else:
+			timer -= _delta
+			if timer <= 0:
+				canPlay = true
+	
+		var movement_force := Vector2(input_direction * move_force, 0.0)
 		apply_central_force(movement_force)
 		
 		if abs(linear_velocity.x) > max_speed:
@@ -26,7 +36,5 @@ func _physics_process(_delta: float) -> void:
 
 	# Small controlled jump
 	if Input.is_action_just_pressed("jump"):
-		if $GroundRay.is_colliding():
-			apply_central_impulse(
-				Vector2(0.0, -jump_force)
-			)
+		if ground_ray.is_colliding():
+			apply_central_impulse(Vector2(0.0, -jump_force))
