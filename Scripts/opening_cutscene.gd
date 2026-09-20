88b3b66192724fcb5302dcf1df938f3edfe797cd
Christian Quin <152404915+enchong-codes@users.noreply.github.com>
2026-09-19 18:10:47 -0600

    ADD: opening_cutscene code

---
+++ Scripts/opening_cutscene.gd
@@ -0,0 +1,35 @@
+extends "res://Scripts/cutscene_player.gd"
+
+func _setup_slides() -> void:
+	slides = [
+		{
+			"text": "That day, I saw her.",
+			"img": null,
+			"audio": null,
+			"time": 3.0
+		},
+		{
+			"text": "But my feet wouldn't move.",
+			"img": null,
+			"audio": null,
+			"time": 3.0
+		},
+		{
+			"text": "Negative thoughts kept filling my head...",
+			"img": null,
+			"audio": null,
+			"time": 3.5
+		},
+		{
+			"text": "...and weighed me down.",
+			"img": null,
+			"audio": null,
+			"time": 3.0
+		},
+		{
+			"text": "She was out of reach.",
+			"img": null,
+			"audio": null,
+			"time": 3.0
+		}
+	]
